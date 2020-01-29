dc0a6fc8b78a3673e23357c81063f32621a3a602
Tobias Høegh <tobias@tujo.no>
2020-01-29 15:35:38 +0100

    update prop to v6 of dnb-ui-lib

--- src/form-demos/form-demo-01.js
+++ src/form-demos/form-demo-01.js
@@ -180,7 +180,7 @@ const MainForm = () => {
             <FormRow>
               <Input
                 label="How much money did you withdraw?"
-                description="Kr"
+                suffix="Kr"
                 value={currentValues.firstInputValue}
                 on_change={({ value: firstInputValue }) =>
                   setValues({
@@ -195,7 +195,7 @@ const MainForm = () => {
             <FormRow>
               <Input
                 label="How much money did you receive?"
-                description="Kr"
+                suffix="Kr"
                 value={currentValues.secondInputValue}
                 on_change={({ value: secondInputValue }) =>
                   setValues({
